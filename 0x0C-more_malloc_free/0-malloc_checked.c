da5d0671c15c4359494eb8bace013fff880f3871
Nwaigba66 <glorianwaigba@gmail.com>
2023-03-16 14:53:26 -0700

    initial commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * *malloc_checked - allocates memory using malloc
@@ -7,14 +7,13 @@
  *
  * Return: a pointer to the allocated memory
  */
-
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
-       
-	ptr = malloc(b);
 
+	ptr = malloc(b);
 	if (ptr == NULL)
-	exit(98);
+		exit(98);
+
 	return (ptr);
 }
