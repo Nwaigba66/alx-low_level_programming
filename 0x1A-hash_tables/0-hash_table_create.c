6174dbe4515df629a73da467d21cffeabda663d2
Nwaigba66 <glorianwaigba@gmail.com>
2023-06-09 11:48:19 -0700

    First commit hash_table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create hash table given size
+ * @size: size
+ * Return: pointer to table; NULL if error
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	 unsigned long int i = 0;
+
+	hash_table_t *hash_table;
+	hash_node_t **array;
+	hash_table = malloc(sizeof(hash_table_t));
+	
+	if (size == 0)
+		return (NULL);
+
+	if (!hash_table)
+		return (NULL);
+	array = malloc(sizeof(*array) * size);
+	if (!array)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		array[i] = NULL;
+		i++;
+	}
+
+	hash_table->size = size;
+	hash_table->array = array;
+
+	return (hash_table);
+}
+
+
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,8 @@
+# C - Hash tables
+
+# What is a hash function?
+- Hash functions have a wide range of applications. They are commonly used in hash tables, cryptographic algorithms,
+ checksums, data integrity verification, digital signatures, and more.
+ The choice of a hash function depends on the specific requirements of the application, including considerations such as speed,
+ collision resistance, security, and distribution of hash values.
+
